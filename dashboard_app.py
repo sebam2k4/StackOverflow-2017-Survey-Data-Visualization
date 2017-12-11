ca1b6fb971107d25638d557c4146763995d28fa6
sebam2k4 <sebam2k4@gmail.com>
2017-12-11 22:09:42 +0000

    Added data route that opens connection to local MongoDB

--- dashboard_app.py
+++ dashboard_app.py
@@ -9,6 +9,11 @@ import json
 
 app = Flask(__name__)
 
+# Info for local connection to MongoDB
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'dashboard_data'
+COLLECTION_NAME = 'stack2017'
 
 # Routes
 @app.route("/")
@@ -18,6 +23,26 @@ def index():
     """
     return render_template("index.html")
 
+@app.route("/data")
+def stack2017_data():
+    """
+    A Flask view to serve project data from
+    MongoDB in JSON format
+    """
+
+
+
+    # Open a connection to MongoDB using a 'with' statement such that the
+    # connection will be closed as soon as we exit the 'with' statement
+    with MongoClient(MONGODB_HOST, MONGODB_PORT) as connection:
+        # Define which collection we wish to access
+        collection = connection[DBS_NAME][COLLECTION_NAME]
+        # Retrieve a result set only with the fields defined in FIELDS
+        # and limit the results to 55000. Using projection redeuces network overhead and processing requirements
+        # by limiting fields that are returned in t results documents.
+        projects = collection.find(projection=FIELDS, limit=55000)
+        # Convert projects to a list in a JSON object and return the JSON data
+        return json.dumps(list(projects))
 
 if __name__ == '__main__':
     app.run(debug=True)
