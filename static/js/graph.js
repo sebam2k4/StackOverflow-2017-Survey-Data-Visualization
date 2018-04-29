0804bf64b8b221f7c4e10ef813d0fc1bbada8e2f
sebam2k4 <sebam2k4@gmail.com>
2018-04-29 20:56:29 +0100

    Remove 'NA' data keys from cahrt groups & reorganize chart code

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,7 +1,6 @@
-// utilize queue libraty for async loading of apis for single analysis
+// utilize queue library for async loading of APIs for single analysis
 // use queue.js to delay rendering the dashboard until the data is loaded from the database
-// fetches data hosted from the api (the Flask server)
-// and passes it into a function.
+// fetches data hosted from the api (the Flask server) and passes it into a function.
 queue()
     .defer(d3.json, "/data")
     .await(makeGraphs);
@@ -15,9 +14,8 @@ function print_filter(filter) {
     console.log(filter + "(" + f.length + ") = " + JSON.stringify(f).replace("[", "[\n\t").replace(/}\,/g, "},\n\t").replace("]", "\n]"));
 }
 
-// if error occurs here then most likely due to
-// server or database problem
-function makeGraphs(error, data) { //later, change back to residentialPurchases
+// if error occurs here then most likely due to server or database problem
+function makeGraphs(error, data) {
     if (error) {
         console.error("makeGraphs error on receiving dataset:", error.statusText);
         throw error;
@@ -25,66 +23,123 @@ function makeGraphs(error, data) { //later, change back to residentialPurchases
 
     // remove loader
     $("#loader").fadeOut();
+
+    /* a method used to filter out "NA" fields from the data before it is
+    charted. It takes a group and produces a 'fake group' which is then passed
+    to dc.js instead of the original group. */
+    function remove_NA_keys(source_group) {
+        return {
+            all:function() {
+                return source_group.all().filter(function(d) {
+                    return d.key != "NA";
+                });
+            }
+        };
+    }
+
+    // function to clear all filters applied on charts and redraw the charts
+    clear_charts = function() {
+        dc.filterAll();
+        dc.renderAll();
+    }
+
+
+    var stack2017 = crossfilter(data);
+
     data.forEach(function (d) {
         /* define the data types & split multiple survey answer strings to arrays */
         d.HaveWorkedLanguage = d.HaveWorkedLanguage.split("; ");
         d.HaveWorkedFramework = d.HaveWorkedFramework.split("; ");
         d.HaveWorkedDatabase = d.HaveWorkedDatabase.split("; ");
-        d.IDE = d.IDE.split("; ");
-        d.Methodology = d.Methodology.split("; ");
-        d.VersionControl = d.VersionControl.split("; ");
         d.DeveloperType = d.DeveloperType.split("; ");
         d.EducationTypes = d.EducationTypes.split("; ");
-        d.CareerSatisfaction = String(d.CareerSatisfaction);
     });
 
-    var stack2017 = crossfilter(data);
-    //var allData = stack2017.groupAll();
-    //var allValues = allData.value()
 
-    // Main Language chart - ordinal scale
-    var languageDim = stack2017.dimension(function (d) {
-        return d.HaveWorkedLanguage;
-    }, true); /* the "true" argument is used in crossfilter v1.4 to indicate
+    /**
+     *  TOTAL SELECTED RESULTS Display
+     */
+    var all = stack2017.groupAll();
+    var numberSelectedND = dc.numberDisplay("#numberSelectedND");
+    numberSelectedND
+    .formatNumber(d3.format(",.0f"))
+    .valueAccessor(function (d) {return d;})
+    .group(all);
+
+
+    /**
+     * DEVELOPER TYPE menu select
+     */
+    var developerTypeDim = stack2017.dimension(function (d) {
+        return d.DeveloperType;
+    }, true); 
+    /* the "true" argument is used in crossfilter v1.4 to indicate
     that the dimension is array-valued */
 
-    var languagesGroup = languageDim.group();
+    var languageByDeveloperTypeGroup = developerTypeDim.group();
+    var languageByDeveloperTypeGroup_filtered = remove_NA_keys(languageByDeveloperTypeGroup);
+    // filter out "NA" data keys from chart
+    var developerTypeMenu = dc.selectMenu("#developerTypeMenu");
+    developerTypeMenu
+        .dimension(developerTypeDim)
+        .group(languageByDeveloperTypeGroup_filtered);
 
+
+    /**
+     *  PROFESSIONAL TYPE menu select
+     */
+    var professionalDim = stack2017.dimension(function (d) {
+        return d.Professional;
+    });
+
+    var languageByProfessionalGroup = professionalDim.group();
+    var professionalMenu = dc.selectMenu("#professional");
+    professionalMenu 
+        .dimension(professionalDim)
+        .group(languageByProfessionalGroup);
+
+
+    /**
+     * LANGUAGE BAR CHART - ordinal scale
+     */ 
+    var languageDim = stack2017.dimension(function (d) {
+        return d.HaveWorkedLanguage;
+    }, true);
+    var languageGroup = languageDim.group();
+    // filter out 'NA' keys from data
+    var languageGroup_filtered = remove_NA_keys(languageGroup);
     var languageChart = dc.barChart("#languageChart");
 
     // get width for initial render of the bar chart. The bar chart is responsive
     // and it's width will adapt to screen width - see code at end
-    var chartWidth = document.getElementById('languageChartContainer').offsetWidth;
+    var chartWidth = $('#languageChartContainer').offsetWidth;
 
     languageChart
-        .width(chartWidth) // see if I can pass in a function that gets the width of the bootstrap container like clo-sm-12 dynamically?
+        .width(chartWidth)
         .height(350)
         .margins({ top: 30, right: 50, bottom: 80, left: 50 })
         .dimension(languageDim)
-        .group(languagesGroup)
+        .group(languageGroup_filtered)
         .transitionDuration(1000)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .elasticY(true)
         .gap(3)
         .renderHorizontalGridLines(true)
-        //.elasticX(true)
+        .title(function(d) {return "";}) // don't show html title tooltip on hover
         .yAxis().ticks(6);
 
-    // Pie Chart - Country
-     /* function to make a string from a game's rating, showing which integer
-    range it falls in */  
+        
+    /**
+     * COUNTRY - pie chart
+     */
     var countryDim = stack2017.dimension(function (d) {
         return d.Country
     });
-
     var countryGroup = countryDim.group();
     // create another group to calculcate percentage affected by other filters
-    var countryGroup2 = countryDim.groupAll().reduceCount();
-
-    var countryChart = dc.pieChart("#CountryPieChart");
-    //console.log(countryGroup.value)
-   // print_filter(countryGroup)
+    var countryPercentGroup = countryDim.groupAll().reduceCount();
+    var countryChart = dc.pieChart("#countryPieChart");
     countryChart
         .height(350)
         .width(350)
@@ -95,24 +150,20 @@ function makeGraphs(error, data) { //later, change back to residentialPurchases
         .slicesCap(10) // max slices to generate (from high to low)
         .label(function(d) { 
             return d.key
-               + ' (' + (d.value / countryGroup2.value() * 100).toFixed(1) + '%)';
+               + ' (' + (d.value / countryPercentGroup.value() * 100).toFixed(1) + '%)';
         })
-        .title(function(d) { 
-            return d.key
-                + ': ' + d.value
-                + ' (' + (d.value / countryGroup2.value() * 100).toFixed(1) + '%)';
-        })
-        //.externalLabels(10)
+        .title(function(d) {return "";}) // don't show html title tooltip on hover
         .othersGrouper(false)
 
 
 
 
-    //Employment Status selector:
+    /**
+     * EMPLOYMENT STATUS menu select
+     */
     var employmentStatusDim = stack2017.dimension(function (d) {
         return d.EmploymentStatus;
     });
-
     var languageByEmploymentStatus = employmentStatusDim.group();
     var employmentStatusMenu = dc.selectMenu("#employmentStatusMenu")
     employmentStatusMenu
@@ -120,46 +171,55 @@ function makeGraphs(error, data) { //later, change back to residentialPurchases
         .group(languageByEmploymentStatus);
 
 
-    //Employment Status selector:
+    /**
+     * FORMAL EDUCATION menu select
+     */
     var formalEducationDim = stack2017.dimension(function (d) {
         return d.FormalEducation;
     });
-
     var languageByFormalEducation = formalEducationDim.group();
     var formalEducationMenu = dc.selectMenu("#formalEducationMenu")
     formalEducationMenu
         .dimension(formalEducationDim)
         .group(languageByFormalEducation);
 
-
-    // Education Type Selector:
+        
+    /**
+     * EDUCATION TYPE (Outside of formal) Selector
+     */
     var educationTypesDim = stack2017.dimension(function (d) {
         return d.EducationTypes;
     }, true);
-
     var languageByEducationTypesGroup = educationTypesDim.group();
-    var educationTypesMenu = dc.selectMenu("#educationTypesMenu")
+    // filter out "NA" data keys from chart
+    var languageByEducationTypesGroup_filtered = remove_NA_keys(languageByEducationTypesGroup);
+    var educationTypesMenu = dc.selectMenu("#educationTypesMenu");
     educationTypesMenu
         .dimension(educationTypesDim)
-        .group(languageByEducationTypesGroup);
+        .group(languageByEducationTypesGroup_filtered);
 
-    //Developer Type selector:
-    var developerTypeDim = stack2017.dimension(function (d) {
-        return d.DeveloperType;
+
+    /**
+     * RELATED LANGUAGES menu select
+     */
+    var relatedLanguagesDim = stack2017.dimension(function(d) {
+        return d.HaveWorkedLanguage;
     }, true);
+    var relatedLanguagesGroup = relatedLanguagesDim.group();
+    // filter out "NA" data keys from chart
+    var relatedLanguagesGroup_filtered = remove_NA_keys(relatedLanguagesGroup);
+    var relatedLanguagesMenu = dc.selectMenu("#relatedLanguages")
+    relatedLanguagesMenu
+        .dimension(relatedLanguagesDim)
+        .group(relatedLanguagesGroup_filtered);
+    
 
-    var languageByDeveloperTypeGroup = developerTypeDim.group();
-    var developerTypeMenu = dc.selectMenu("#developerTypeMenu")
-    developerTypeMenu
-        .dimension(developerTypeDim)
-        .group(languageByDeveloperTypeGroup);
-
-
-    //Major Undergrad selector:
+    /**
+     * UNDERGRADUATE MAJOR menu select
+     */
     var majorUndergradDim = stack2017.dimension(function (d) {
         return d.MajorUndergrad;
     });
-
     var languageByMajorUndergradGroup = majorUndergradDim.group();
     var majorUndergradMenu = dc.selectMenu("#majorUndergradMenu")
     majorUndergradMenu
@@ -167,32 +227,23 @@ function makeGraphs(error, data) { //later, change back to residentialPurchases
         .group(languageByMajorUndergradGroup);
 
 
-    // Career Satisfaction Selector:
-    var careerSatisfactionDim = stack2017.dimension(function (d) {
-        return d.CareerSatisfaction;
-    }, true);
-
-    var languageByCareerSatisfactionGroup = careerSatisfactionDim.group();
-    var careerSatisfactionMenu = dc.selectMenu("#careerSatisfactionMenu")
-    careerSatisfactionMenu
-        .dimension(careerSatisfactionDim)
-        .group(languageByCareerSatisfactionGroup);
-
-
-    // Database chart:
+    /**
+     * DATABASES row chart
+     */
     var databaseDim = stack2017.dimension(function (d) {
         return d.HaveWorkedDatabase;
     }, true);
 
     var languageByDatabaseGroup = databaseDim.group();
-
+    // filter out "NA" data keys from chart
+    var languageByDatabaseGroup_filtered = remove_NA_keys(languageByDatabaseGroup)
     var databaseChart = dc.rowChart("#databaseChart");
 
     databaseChart
         .width(400)
         .height(250)
         .dimension(databaseDim)
-        .group(languageByDatabaseGroup)
+        .group(languageByDatabaseGroup_filtered)
         .ordinalColors(["#1f77b4"])
         .transitionDuration(1000)
         .othersGrouper(false)
@@ -200,19 +251,24 @@ function makeGraphs(error, data) { //later, change back to residentialPurchases
         .xAxis().ticks(4);
 
 
-    //Framework chart:
+    /**
+     * FRAMEWORKS row chart
+     */
     var frameworkDim = stack2017.dimension(function (d) {
         return d.HaveWorkedFramework;
     }, true)
 
     var languageByFrameworkGroup = frameworkDim.group();
+    // filter out "NA" data keys from chart
+    var languageByFrameworkGroup_filtered = remove_NA_keys(languageByFrameworkGroup)
+
     var frameworkChart = dc.rowChart("#frameworkChart");
 
     frameworkChart
         .width(400)
         .height(250)
         .dimension(frameworkDim)
-        .group(languageByFrameworkGroup)
+        .group(languageByFrameworkGroup_filtered)
         .ordinalColors(["#1f77b4"])
         .transitionDuration(1000)
         .othersGrouper(false)
@@ -220,42 +276,21 @@ function makeGraphs(error, data) { //later, change back to residentialPurchases
         .xAxis().ticks(4);
 
 
-    // total selected results Display
-    var all = stack2017.groupAll();
-    var numberSelectedND = dc.numberDisplay("#numberSelectedND");
-    numberSelectedND
-    .formatNumber(d3.format(",.0f"))
-    .valueAccessor(function (d) {return d;})
-    .group(all);
 
-    // related languages menu select
-    var relatedLanguagesDim = stack2017.dimension(function(d) {
-        return d.HaveWorkedLanguage;
-    }, true);
-
-    var relatedLanguagesGroup = relatedLanguagesDim.group();
-    var relatedLanguagesMenu = dc.selectMenu("#relatedLanguages")
-    relatedLanguagesMenu
-        .dimension(relatedLanguagesDim)
-        .group(relatedLanguagesGroup);
-
-
-
-
-    // make charts responsive on window resize
+    /* make the Languages bar chart responsive on window resize */
     // adapted from: https://css-tricks.com/snippets/jquery/done-resizing-event/
     var resizeTimeout;
     window.onresize = function() {
         clearTimeout(resizeTimeout);
-        // resize and redraw the charts after a short delay once window resizing stops
+        // resize and redraw the chart after a short delay once window resizing stops
         resizeTimeout = setTimeout(function() {
             // recalculate chart width
             var newChartWidth = document.getElementById('languageChartContainer').offsetWidth;
 
-            // set new width and redraw charts
+            // set new width and redraw the chart
             languageChart
                 .width(newChartWidth)
-                .rescale() // resets the scale and bars widths + bar margins (gaps)
+                .rescale() // resets the scale and bars widths & bar margins (gaps between bars)
                 .redraw(); 
 
         }, 150);
@@ -263,28 +298,41 @@ function makeGraphs(error, data) { //later, change back to residentialPurchases
     
     dc.renderAll();
 
-    /* TOOLTIPS */
 
+    /* TOOLTIPS */
     // initialize tooltips for languages bar chart tooltip
     var bar_tooltip = d3.tip()
         .attr('class', 'd3-tip')
         .offset([-10, 0])
         .html(function(d) {
             return "<strong>" + d.data.key + ":</strong> <span style='color:red'>" + d.data.value + "</span>";
-        })
+        });
 
     d3.selectAll("#languageChart .bar")
         .call(bar_tooltip)
         .on('mouseover', bar_tooltip.show)
         .on('mouseout', bar_tooltip.hide);
 
+    // initialize tooptips for country pie chart
+    var pie_tooltip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([10, 0])
+        .html(function(d) { 
+            return "<strong>" +d.data.key + ":</strong> <span style='color:red'>" + d.value + " (" + (d.value / countryPercentGroup.value() * 100).toFixed(1) + "%)";
+        });
+
+    d3.selectAll("#countryPieChart .pie-slice")
+    .call(pie_tooltip)
+    .on('mouseover', pie_tooltip.show)
+    .on('mouseout', pie_tooltip.hide);
+    
     // initialize tooltips for database and frameworks row charts
     var row_tooltip = d3.tip()
         .attr('class', 'd3-tip')
         .offset([-10, 0])
         .html(function(d) {
             return "<strong>" + d.key + ":</strong> <span style='color:red'>" + d.value + "</span>";
-        })
+        });
 
     d3.selectAll("#databaseChart .row")
         .call(row_tooltip)
